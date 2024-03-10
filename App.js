92020566277ef0efdbd3ff6e7e42744687dd6184
harish <harishshotmail@gmail.com>
2024-03-10 22:13:33 +0530

    changes made

--- App.js
+++ App.js
@@ -1,21 +1,5 @@
-/***
-<div id="parent">
-    <div id="child1">
-        <h1>I'm an h1 tag</h1>
-        <h2>I'm an h2 tag</h2>
-    </div>
-    <div id = "child2">
-        <h1> I'm an h1 tag </h1>
-        <h2> I'm an h2 tag </h2>
-    </div>
-<div/>
- * 
- * 
- * 
- * 
- * 
- */
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement(
     "div", 
     {id: "parent"}, 
@@ -35,11 +19,6 @@ const parent = React.createElement(
     ]
 );
 
-// const heading = React.createElement(
-//     "h1", 
-//     {id: "heading", xyz: "abc"}, 
-//     "Hello World From React!"
-// );
 console.log(parent); //object
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent)
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 This is React Developement.
+
+
+
+
+* Parcel -- usages
+
+- Dev Build, 
+- Set up local server (localhost:1234),
+- HMR - Hot Module Replacement (auto refresh on saving code)'
+- File watching algorithm - written C++
+- Caching - Faster build(it will take space for caching .parcel-cache)
+- Image optimization
+- Minification (while going for prod)
+- Bundling
+- Compress
+- Consistant Hashing
+- Code Splitting
+- Differential Bundling
+- Diagnotics(will throw proper error logs)
+- Error Handling
+- HTTPS
+- Tree shaking -- remove unused code
+
+// check parceljs.org for more info 
