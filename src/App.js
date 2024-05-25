4fb7d7ed06fbfc75df6fc7d20e8d83fd6e80e88b
harish <harishshotmail@gmail.com>
2024-05-25 19:26:57 +0530

    Added online status and lazy loading functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -6,11 +6,12 @@ import Aboutus from "./components/Aboutus";
 import Cart from "./components/Cart";
 import Contactus from "./components/Contact";
 import ErrorBoundary from "./components/Error";
-import Menu from "./components/Menu"
+import Menu from "./components/Menu";
+// import Grocery from "./components/Grocery";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+// import Grocery from "./components/Grocery";
 
-
-
+const Grocery = lazy(() => import("./components/Grocery"));
 const AppLayout = () => {
   return (
     <div className="app">
@@ -20,35 +21,43 @@ const AppLayout = () => {
   );
 };
 
-
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <ErrorBoundary/>,
+    errorElement: <ErrorBoundary />,
     children: [
       {
         path: "/",
-        element: <Body />
+        element: <Body />,
       },
       {
         path: "aboutus",
-        element: <Aboutus />
+        element: <Aboutus />,
       },
       {
         path: "contactus",
-        element: <Contactus />
+        element: <Contactus />,
       },
       {
         path: "cart",
-        element: <Cart />
+        element: <Cart />,
       },
       {
         path: "restaurant/:resId",
-        element: <Menu/>
-      }
+        element: <Menu />,
+      },
+      {
+        path: "grocery",
+        element: (
+          <Suspense fallback={"Waiting for Grocery component to load"}> 
+            <Grocery/>
+          </Suspense>
+        )
+        // fallback is something which will help to display before content get's loaded
+      },
     ],
-  }
+  },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router}/>);
+root.render(<RouterProvider router={router} />);
